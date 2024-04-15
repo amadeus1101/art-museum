516c7cccbfffc69c80c9f14eb60ed9630a2e7986
Artemy-sweden <ghosteagle2020@mail.ru>
2024-04-15 09:06:52 +0300

    bookmark_interactive_added

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,9 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { theme } from '../constants/theme';
 import { devices } from '../constants/devices';
+import { useState } from 'react';
 import bookmark_icon from '../assets/img/icon-bookmark-2.png';
+import bookmark_active from '../assets/img/bookmark-checked.png';
 
-const BookmarkStyles = styled.div`
+const BookmarkStyles = styled.div<{ active: boolean }>`
   width: 59px;
   height: 59px;
   display: flex;
@@ -12,6 +14,11 @@ const BookmarkStyles = styled.div`
   background: #f9f9f9;
   border-radius: 50%;
   transition: 0.3s;
+  ${(props) =>
+    props.active &&
+    css`
+      background: #f17900;
+    `}
   &:hover {
     cursor: pointer;
     background: rgba(251, 215, 178, 100%);
@@ -39,9 +46,13 @@ const BookmarkStyles = styled.div`
 `;
 
 const Bookmark = () => {
+  const [bookmarkActive, setBookmarkActive] = useState(false);
+  const onClickBookmark = () => {
+    setBookmarkActive(!bookmarkActive);
+  };
   return (
-    <BookmarkStyles>
-      <img src={bookmark_icon} alt="bookmark-icon" />
+    <BookmarkStyles active={bookmarkActive} onClick={() => onClickBookmark()}>
+      <img src={!bookmarkActive ? bookmark_icon : bookmark_active} alt="bookmark-icon" />
     </BookmarkStyles>
   );
 };
