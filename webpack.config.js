c62fcfa41ff21d6134f4e1fc3c404b1135a91ebf
Artemy-sweden <ghosteagle2020@mail.ru>
2024-04-21 18:18:00 +0300

    try_babel_fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./index.js",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
-  },
-  target: "web",
-  devServer: {
-    port: "5000",
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
+	entry: ['@babel/polyfill', './index.js'],
+	mode: 'development',
+	devtool: 'inline-source-map',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+	},
+	target: 'web',
+	devServer: {
+		port: '5000',
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		open: true,
+		hot: true,
+		liveReload: true,
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
 };
