daf72170a7b66a9b31dd262e1b614342c1e0300a
Artemy-sweden <ghosteagle2020@mail.ru>
2024-04-22 00:05:55 +0300

    Styled_components_&_axios_&_jest_router

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
