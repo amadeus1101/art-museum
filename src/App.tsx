5943179683f31506e4565be874553b9a7518fb34
Artemy-sweden <ghosteagle2020@mail.ru>
2024-04-22 07:53:52 +0300

    PUBLIC_FOLDER

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 const App = () => {
-  return (
-    <div>
-      <h1>Create React App from Scratch Using Typescript..!!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Create React App from Scratch Using Typescript..!!</h1>
+		</div>
+	);
 };
 export default App;
